51ee924f8f6625279fbb5ca5ac4bd49f941a1d93
Phyu Phyu Hlaing <phyuphyu.h27@gmail.com>
2025-02-07 14:14:46 +0700

    openweathermap icon

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Weather = () => {
-  const [city, setCity] = useState('Bangkok');
+  const [city, setCity] = useState('');
   const [weather, setWeather] = useState(null);
   const [date, setDate] = useState('');
   const [lat, setLat] = useState(null);
@@ -10,17 +10,18 @@ const Weather = () => {
 
   const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
 
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        setLat(position.coords.latitude);
-        setLon(position.coords.longitude);
-      },
-      (error) => {
-        console.error('Error getting location:', error);
-      }
-    );
-  }, []);
+  // useEffect(() => {
+  //   navigator.geolocation.getCurrentPosition(
+  //     (position) => {
+  //       setLat(position.coords.latitude);
+  //       setLon(position.coords.longitude);
+  //     },
+  //     (error) => {
+  //       console.error('Error getting location:', error);
+  //       setCity('Bangkok');
+  //     }
+  //   );
+  // }, []);
 
   useEffect(() => {
     if (lat && lon) {
@@ -94,15 +95,37 @@ const Weather = () => {
   return (
     <div>
       <h1 className="text-3xl font-bold">Weather App</h1>
-      <form onSubmit={handleSubmit}>
-        <input type="text" value={city} onChange={handleCityChange} placeholder='Enter city name' />
-        <button type='submit'>Get Weather</button>
+      <form
+          onSubmit={handleSubmit}
+          className='bg-white-100 p-1 rounded-lg shadow-md mb-4 flex divide-x divide-blue-200'
+      >
+        <input
+            type="text"
+            value={city}
+            onChange={handleCityChange}
+            placeholder='Enter city name'
+            className="p-2 w-64 flex-1"
+        />
+        <button
+            type='submit'
+            className='w-32 flex-none cursor-pointer hover:text-sky-500'
+        >
+          Get Weather
+        </button>
       </form>
-      <button onClick={handleGetLocation}>Current Location</button>
+      <button
+          onClick={handleGetLocation}
+          className='bg-blue-500 hover:bg-blue-600'
+      >
+      </button>
       {weather ? (
         <div>
           <h2>Weather in {weather.name}</h2>
           <p>{date}</p>
+          <img
+            src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+            alt={weather.weather[0].description}
+          />
           <p>{weather.weather[0].description}</p>
           <p>Temperature: {weather.main.temp}°C</p>
           <p>Humidity: {weather.main.humidity}%</p>
